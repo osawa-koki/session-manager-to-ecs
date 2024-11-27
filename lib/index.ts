e4d6ce4d840843f53148cbda814ff000babfde2f
osawa-koki <koki@osawa.dev>
2024-11-27 21:37:29 +0900

    ECSのプロビジョニング。

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+import NetworkStack from './network/network';
+import ComputeStack from './compute/compute';
+import OutputStack from './output/output';
+
 export class IndexStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, {
       ...props,
       stackName: process.env.BASE_STACK_NAME!,
     });
+
+    const networkStack = new NetworkStack(this, 'NetworkStack', {
+      stackName: `${process.env.BASE_STACK_NAME!}-network`,
+    });
+
+    const computeStack = new ComputeStack(this, 'ComputeStack', {
+      stackName: `${process.env.BASE_STACK_NAME!}-compute`,
+      vpc: networkStack.vpc,
+    });
+    computeStack.addDependency(networkStack);
+
+    const outputStack = new OutputStack(this, 'OutputStack', {
+      stackName: `${process.env.BASE_STACK_NAME!}-output`,
+      alb: computeStack.fargateService,
+    });
+    outputStack.addDependency(computeStack);
   }
 }
